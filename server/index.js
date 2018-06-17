f8261c820182715b259e9e60800ac1d05be78927
Lucas Trindade <lucasktrindade@gmail.com>
2018-06-17 12:09:16 -0300

    Altera server/index.js para testes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,10 @@
-import app from '../src/app';
-import db from'../src/models';
-import config from '../config/config';
+var express = require('express');
+var app = express();
 
-app.listen(3000, () => {
-  db.sequelize.sync();
-  console.log('Running on port 3000...');
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
 
-console.log('Executing Server: index.js ...');
-console.log('');
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
